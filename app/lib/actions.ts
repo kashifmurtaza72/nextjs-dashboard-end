6770d44d8f200e07e16d3eb0a34feee2b70bc8ca
kashifmurtaza72 <kashifmurtaza72@gmail.com>
2025-10-13 16:38:19 +0500

    aaa

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,36 @@
-'use server';
-//export async function createInvoice(formData: FormData) {}
-export async function createInvoice(formData : FormData) {
-    
+"use server";
+// import { z } from 'zod';
+
+// const FormSchema = z.object({
+//   id: z.string(),
+//   customerId: z.string(),
+//   amount: z.coerce.number(),
+//   status: z.enum(['pending', 'paid']),
+//   date: z.string(),
+// });
+
+import { z } from "zod";
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoicee = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  //   const rawFormData = {
+  //     customerId: formData.get("customerId"),
+  //     amount: formData.get("amount"),
+  //     status: formData.get("status"),
+  //   };
+  //   console.log(typeof rawFormData.amount);
+
+  const { customerId, amount, status } = CreateInvoicee.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
 }
