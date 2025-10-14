f1b75d919f8aec1c35f9be502ec727b9279e65e3
kashifmurtaza72 <70048358+kashifmurtaza72@users.noreply.github.com>
2025-10-14 22:41:08 +0500

    eee

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -16,13 +16,6 @@ const FormSchema = z.object({
 const CreateInvoicee = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  //   const rawFormData = {
-  //     customerId: formData.get("customerId"),
-  //     amount: formData.get("amount"),
-  //     status: formData.get("status"),
-  //   };
-  //   console.log(typeof rawFormData.amount);
-
   //Given any Zod schema 'FormSchema', use .parse to validate an input
   //When validation fails, the .parse() method will throw a ZodError
   const { customerId, amount, status } = CreateInvoicee.parse({
