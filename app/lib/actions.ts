479b48481d7dbcd6806d5a19b8a620017536fb84
kashifmurtaza72 <kashifmurtaza72@gmail.com>
2025-10-21 17:00:17 +0500

    usact

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,25 +6,63 @@ import { redirect } from "next/navigation";
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 // https://www.npmjs.com/package/zod and //https://zod.dev/
 //define a zod schema
+// const FormSchema = z.object({
+//   id: z.string(),
+//   customerId: z.string(),
+//   amount: z.coerce.number(),
+//   status: z.enum(["pending", "paid"]),
+//   date: z.string(),
+// });
+
+
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["pending", "paid"]),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
+
+
+
+export type State = {
+  message?: string | null;
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+};
+
 //https://v3.zod.dev/?id=pickomit  omit means skip/ignore during validation
 const CreateInvoiceData = FormSchema.omit({ id: true, date: true });
 const UpdateInvoiceData = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
   //Given any Zod schema 'FormSchema', use .parse to validate an input
   //When validation fails, the .parse() method will throw a ZodError
-  const { customerId, amount, status } = CreateInvoiceData.parse({
+  const validatedFields = CreateInvoiceData.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
+
+   // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
   try {
@@ -70,8 +108,8 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-   throw new Error('Failed to Delete Invoice');
-   // if put throw new Error, then unreachable code block below
+  throw new Error("Failed to Delete Invoice");
+  // if put throw new Error, then unreachable code block below
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
